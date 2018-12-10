f231c017b15603be44eecd075e5282ad65b893b4
wangsai <wonsaign@yeah.net>
2018-12-10 18:08:05 +0800

    rename application

--- zhongda-core/core-product/src/main/java/com/zhongda/demo/CoreProductApplication.java
+++ zhongda-core/core-product/src/main/java/com/zhongda/demo/CoreProductApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZhongdaAdminApplication {
+public class CoreProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZhongdaAdminApplication.class, args);
+		SpringApplication.run(CoreProductApplication.class, args);
 	}
 }
--- zhongda-ks/src/main/java/com/zhongda/demo/ZhongdaKSApplication.java
+++ zhongda-ks/src/main/java/com/zhongda/demo/ZhongdaKSApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZhongdaAdminApplication {
+public class ZhongdaKSApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZhongdaAdminApplication.class, args);
+		SpringApplication.run(ZhongdaKSApplication.class, args);
 	}
 }
