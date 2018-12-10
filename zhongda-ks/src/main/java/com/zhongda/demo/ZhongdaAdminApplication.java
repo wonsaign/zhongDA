afb8536de90cf5f366cbe41291ec71cc5ee5ffb5
wangsai <wonsaign@yeah.net>
2018-12-10 18:04:26 +0800

    add spring boot

---
+++ zhongda-ks/src/main/java/com/zhongda/demo/ZhongdaAdminApplication.java
@@ -0,0 +1,12 @@
+package com.zhongda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhongdaAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhongdaAdminApplication.class, args);
+	}
+}
