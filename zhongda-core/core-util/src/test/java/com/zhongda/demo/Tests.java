114fffa08c6c13aa3f72615bd53bfd4e5cd86eb3
wangsai <wonsaign@yeah.net>
2018-12-10 18:20:11 +0800

    add yesu

--- zhongda-core/core-util/src/test/java/com/zhongda/demo/Tests.java
+++ zhongda-core/core-util/src/test/java/com/zhongda/demo/Tests.java
@@ -7,10 +7,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ZhongdaAdminApplicationTests {
+public class Tests {
 
 	@Test
 	public void contextLoads() {
+		System.out.println("我是测试");
 	}
 
 }
